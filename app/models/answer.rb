5200aebf5b07415f726cf60656fe269a62242a27
Achilles-Dev <solohag92@gmail.com>
2022-09-07 17:29:22 +0000

    Update models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,6 @@
 class Answer < ApplicationRecord
   belongs_to :question
+  belongs_to :subject
 
   validates :answer_no, presence: true,
                         numericality: { greater_than_or_equal_to: 1, less_than: BigDecimal(101) }
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ApplicationRecord
+  belongs_to :subject
+
   validates :year, presence: true, inclusion: { in: 1900..Date.today.year },
                    format: {
                      with: /(19|20)\d{2}/i,
