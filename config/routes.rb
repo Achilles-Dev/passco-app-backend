2f4cdd53bd193ecdc22d2c2cf1482154c575758f
Achilles-Dev <solohag92@gmail.com>
2022-09-28 20:21:29 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :show]
+      resources :users, except: [:create]
       resources :questions
       resources :answers
       resources :user_data, except: [:update]
