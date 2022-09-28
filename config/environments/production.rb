692bc85ea7a8cbb8d3524aa4a09adccad5dbb397
Achilles-Dev <solohag92@gmail.com>
2022-09-28 10:58:09 +0000

    Update production ervironment file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 require "active_support/core_ext/integer/time"
 
+Rails.application.routes.default_url_options[:host] = 'https://passco-app-backend.herokuapp.com'
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
